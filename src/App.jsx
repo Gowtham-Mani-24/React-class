e5eabfbacf232e44ef1456163db4b15e630259b4
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-08-09 10:48:28 +0530

    added array list using traditional way

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-function App() {
+function App({notes}) {
+  // console.log(props);
+  let noteList = [];
+  for (let i=0;i<notes.length;i++){
+    noteList.push(<li>{notes[i].content}</li>);
+  }
+  console.log(noteList);
   return (
     <div>
-      <h1>Hello Junga</h1>
+      <h1>Notes</h1>
+     <ul>
+      {/* <li>{notes[0].content}</li>
+      <li>{notes[1].content}</li>
+      <li>{notes[2].content}</li> */}
+      {/* for dynamically get the data use for loop */}
+        {
+          noteList
+        }
+     </ul>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
+const notes = [
+  {
+    id:1,
+    content:'HTML is easy',
+    data:'2015-05-12',
+    important:true
+  },
+  {
+    id:2,
+    content:'HTML is medium',
+    data:'2015-06-12',
+    important:false
+  },
+  {
+    id:3,
+    content:'HTML is Hard',
+    data:'2015-07-12',
+    important:true
+  },
+  {
+    id:4,
+    content:'HTML is Excellent',
+    data:'2015-08-12',
+    important:true
+  },
+]
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App
+    notes = {notes} //props
+    />
+    
 )
