2dcfd0ce2790dae668ba27f763e3f0cd0732c339
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-08-10 14:06:57 +0530

    added list items into table

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
-function App() {
+function App({ items }) {
+  console.log(items);
   return (
     <div>
-      <h1>Hello Junga</h1>
+      <table style={{border:'1px solid black', borderCollapse:'collapse'}}>
+        <thead>
+          <tr>
+          {
+              Object.keys(items[0]).map((topic, index) => {
+                return (
+                  <td style={{border:'1px solid black',padding:'20px'}} key={index}>
+                    {topic.toUpperCase()}
+                  </td>
+                );
+              })
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            items.map(item => {
+              return <tr style={{border:'1px solid black'}} key={item.id}>
+                 {
+                   Object.values(item).map((value,index) =>{
+                     return <td style={{border:'1px solid black',padding:'20px'}} key={index}>{value}</td>
+                   })
+                 }
+               </tr>
+             })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const items =[
+  {id:1, product: "Mobile", price: 10000},
+  {id:2, product: "Laptop", price: 50000},
+  {id:3, product: "Tablet", price: 1500},
+  {id:4, product: "Smartwatch", price: 5000}
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App 
+    items={items}
+    />
+  
 )
