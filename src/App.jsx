3af10b68eee2fadf50ab3c7bfa1b5b8dc05a9219
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-08-10 22:18:32 +0530

    base for class componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      <h1>Hello Junga</h1>
-    </div>
-  )
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Junga</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
