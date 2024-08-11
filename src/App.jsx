b6114dcb6dc7a5bffcc7d5bcc855e4f7badff9be
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-08-11 11:50:45 +0530

    added constructor a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+ constructor () {
+  super(); // calling th e parent constructor
+  this.state = {
+    name:"Leo"
+  }
+  }
   render() {
+    console.log(this.state);
     return (
       <div>
-        <h1>Junga</h1>
+        <h1>Hello {this.state.name}</h1>
       </div>
     )
   }
