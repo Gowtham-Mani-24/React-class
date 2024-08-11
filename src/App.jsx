a119425da88b1d2ef441295cb6627993a84c9ebb
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-08-11 12:00:49 +0530

    added a button to update name using setstate

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,28 @@ export class App extends Component {
  constructor () {
   super(); // calling th e parent constructor
   this.state = {
-    name:"Leo"
+    name:"Leo dass"
   }
   }
+
+    handleClick = () => {
+      // should not mutate the state directly
+    //this.state.name = 'Harold'
+    this.setState({
+      name:'harold dass'
+    })
+    console.log(this.state);
+  }
+
+ 
   render() {
     console.log(this.state);
+
+    
     return (
       <div>
         <h1>Hello {this.state.name}</h1>
+        <button onClick={this.handleClick}>Update Name</button>
       </div>
     )
   }
