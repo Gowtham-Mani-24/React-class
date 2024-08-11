85db739c53290861e446dcb58dabfa8b27bf1d60
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-08-11 13:17:59 +0530

    used hook to do counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [name,setName] = useState('Leo Dass');
+  const [count,updateCount] = useState(0);
   
-  function handleClick  ()  {
+  function incClick  ()  {
     //console.log('button is clicked');
-    setName('Antony Dass')
+    updateCount(count + 1);
+  }
+
+  function decClick (){
+    updateCount(count-1);
+  }
+
+  function resClick (){
+    updateCount(0);
   }
 
   return (
     <div>
-      <h1>Hello {name}</h1>
-      <button onClick={handleClick}>Update Name</button>
+      <h1>Count: {count}</h1>
+      <button onClick={incClick}>Increment</button>
+      <button onClick={decClick}>Decrement</button>
+      <button onClick={resClick}>Reset</button>
     </div>
   )
 }
