548b9938d703c3556e96acb27ecba85d1a078124
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-08-13 23:11:02 +0530

    router dom context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Login from './Components/Login';
 
- function App() {
+const App = () => {
+
+  const [page, setPage] = useState('home');
+
+  const toPage = (page) => (e)  =>{
+    e.preventDefault();
+    setPage(page);
+  }
+
+  const content = () => {
+    if(page === 'login') {
+      return <Login />
+    }
+  }
+
   return (
     <div>
-      <h1>Junga</h1>
+      <h1>Home Page</h1>
+      <a href='/login' onClick={toPage('login')}>Login</a>
+      {content()}
     </div>
   )
 }
